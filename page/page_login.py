f2878ac9f1639832227103f3ab1fcb6e15a47601
heessun <110523188+heessun@users.noreply.github.com>
2025-12-08 18:01:04 +0900

    Update page_login.py
    test

--- page/page_login.py
+++ page/page_login.py
@@ -1,1 +1,18 @@
 
+# pages/login_page.py
+from playwright.sync_api import Page
+
+class LoginPage:
+    def __init__(self, page: Page):
+        self.page = page
+        self.username = 'input[placeholder="이메일"]'
+        self.password = 'input[placeholder="비밀번호"]'
+        self.login_btn = "text=로그인"
+
+    def goto(self, url):
+        self.page.goto(url)
+
+    def login(self, user, pwd):
+        self.page.fill(self.username, user)
+        self.page.fill(self.password, pwd)
+        self.page.click(self.login_btn)
