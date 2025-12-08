faa1bf919aa911445fc1a5a3237414cec25bfbd8
heessun <110523188+heessun@users.noreply.github.com>
2025-12-08 18:03:39 +0900

    Update login.py
    test

--- testcase/login.py
+++ testcase/login.py
@@ -1,1 +1,24 @@
-test
+# tests/test_login.py
+import pytest
+from qaseio.pytest import qase
+from playwright.sync_api import sync_playwright
+from pages.login_page import LoginPage
+
+@pytest.fixture
+def browser():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context()
+        page = context.new_page()
+        yield page
+        context.close()
+        browser.close()
+
+@qase.id(1)  # Qase 테스트 케이스 ID
+def login_success(browser):
+    login = LoginPage(browser)
+
+    login.goto("https://asm6.estsecurity.com/auth/login")
+    login.login("heesun@estsecurity.com", "estsoft1!")
+
+    assert browser.url == "https://asm6.estsecurity.com/dashboard"
